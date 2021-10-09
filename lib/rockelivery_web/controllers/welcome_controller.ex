c01c0085cca14a8b72d548afb78dda97258753a9
Renato Marinho <renato.marinhofr@gmail.com>
2021-10-09 12:13:14 -0300

    Criando a migration do User

--- lib/rockelivery_web/controllers/welcome_controller.ex
+++ lib/rockelivery_web/controllers/welcome_controller.ex
@@ -1,10 +1,9 @@
 defmodule RockeliveryWeb.WelcomeController do
   use RockeliveryWeb, :controller
 
-  def index(conn, params) do
+  def index(conn, _params) do
     conn
     |> put_status(:ok)
-    |> text("Welcome :)")
-    |> IO.inspect()
+    |> text("Welcome!")
   end
 end
