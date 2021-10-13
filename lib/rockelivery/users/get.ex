e047c4776f5d3b7e3498cf54b8a04962c0f7607f
Renato Marinho <renato.marinhofr@gmail.com>
2021-10-13 11:53:12 -0300

    Criando um módulo para leitura de usuários

---
+++ lib/rockelivery/users/get.ex
@@ -0,0 +1,30 @@
+defmodule Rockelivery.Users.Get do
+  alias Ecto.UUID
+  alias Rockelivery.{Repo, User}
+
+  # Usando with e else:
+
+  # def by_id2(id) do
+  #   with {:ok, uuid} <- UUID.cast(id),
+  #        %User{} = user <- Repo.get(User, uuid) do
+  #     {:ok, user}
+  #   else
+  #     :error -> {:error, %{status: :not_found, result: "Invalid id format"}}
+  #     nil -> {:error, %{status: :not_found, result: "User not found"}}
+  #   end
+  # end
+
+  def by_id(id) do
+    case(UUID.cast(id)) do
+      :error -> {:error, %{status: :not_found, result: "Invalid id format"}}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, %{status: :not_found, result: "User not found"}}
+      user -> {:ok, user}
+    end
+  end
+end
